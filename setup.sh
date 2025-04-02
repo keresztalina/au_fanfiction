ecc5065f9a4be5f307a49bcd72eca6fc6ef8e608
keresztalina <76069299+keresztalina@users.noreply.github.com>
2025-04-02 12:50:24 +0200

    reqs update

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 git config --global user.name "keresztalina"
 git config --global user.email "76069299+keresztalina@users.noreply.github.com"
 pip install -r requirements.txt
-python -m spacy download en_core_web_md
+#python -m spacy download en_core_web_md
