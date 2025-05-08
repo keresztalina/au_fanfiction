08d2bd1fbe0fdd5f5be83c3e514f4b69144be5b2
keresztalina <76069299+keresztalina@users.noreply.github.com>
2025-05-08 11:04:20 +0200

    modeling progress

--- src/topic_model.py
+++ src/topic_model.py
@@ -8,25 +8,44 @@ from sentence_transformers import SentenceTransformer
 from turftopic import KeyNMF
 
 def main(num_topics: int, top_n: int):
-    # Load corpus
-    with open(os.path.join("obj", "corpus_ner.pkl"), "rb") as f:
+
+    # file paths
+    corpus_path = os.path.join("obj", "corpus_ner.pkl")
+    keywords_path = os.path.join("obj", f"keywords_{top_n}.pkl")
+    model_path = os.path.join("obj", f"ner_{num_topics}_{top_n}.pkl")
+
+    # load corpus
+    with open(corpus_path, "rb") as f:
         corpus = pickle.load(f)
 
-    # Load embeddings
+    # load pre-computed embeddings
     embeddings = np.load(os.path.join("obj", "embeddings.npy"))
 
-    # Setup vectorizer and encoder
+    # setup vectorizer and encoder
     vectorizer = TfidfVectorizer(stop_words="english", max_df=0.9)
     encoder = SentenceTransformer("paraphrase-mpnet-base-v2")
 
-    # Fit KeyNMF
+    # fit KeyNMF
     model = KeyNMF(num_topics, top_n=top_n, vectorizer=vectorizer, encoder=encoder)
+
+    # retrieve keywords
+    # check if existing keywords matrix exists for this n keywords, otherwise create one
+    #if os.path.exists(keywords_path):
+    #    with open(keywords_path, "rb") as f:
+    #        keywords = pickle.load(f)
+    #    print("Found pre-computed keywords matrix! Proceeding with topic discovery...")
+    #else:
+    #    print("No pre-computed keywords matrix found! Proceeding with keyword extraction...")
+    #    keywords = model.extract_keywords(corpus)
+    #    with open(keywords_path, "rb") as f:
+    #        pickle.dump(keywords, f)
+
+    # discover topics
     topic_data = model.prepare_topic_data(corpus, embeddings=embeddings)
 
-    # Pickle result
-    filename = os.path.join("obj", f"ner_{num_topics}_{top_n}.pkl")
-    with open(filename, "wb") as file:
-        pickle.dump(topic_data, file)
+    # save model
+    with open(model_path, "wb") as f:
+        pickle.dump(topic_data, f)
 
     print(f"Saved topic data to {filename}")
 
